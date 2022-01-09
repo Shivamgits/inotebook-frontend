84b3f085466665a341240267b6013f614ff72745
shivam singh <92594066+Shivamgits@users.noreply.github.com>
2022-01-09 14:43:45 +0530

    adding NoteItem in a Seperate Note Component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import './App.css';
+import Home from './components/Home';
 import Navbar from './components/Navbar';
+import NoteState from './context/notes/NoteState';
 
 function App() {
   return (
    <>
+   <NoteState>
    <Navbar/>
+   <div className="container">
+   <Home/>
+   </div>
 
 
-   
+   </NoteState>
    </>
   );
 }
